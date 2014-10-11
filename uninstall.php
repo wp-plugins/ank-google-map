80ec2eabe856896acd15db1fa452ba8ecc9ebc96
ank91 <ank91@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-11 16:25:23 +0000

    updated trunk for 1.5.2
    git-svn-id: https://plugins.svn.wordpress.org/ank-google-map/trunk@1005707 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -3,10 +3,13 @@
 /* this file will be used to remove all traces of this plugin when uninstalled */
 
 //if uninstall not called from WordPress do exit
+
 if( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
     exit;
 
-//lets clean that row from database if exists
+/*
+ * remove the database entry created by this plugin
+ */
 if(get_option( 'ank_google_map' ) ) {
     delete_option('ank_google_map');
 }
